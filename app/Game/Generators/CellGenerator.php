1c66055d277fece1de7d4a5887adeeb1b13c89a3
bezmenovd <d28829192@gmail.com>
2025-02-25 00:09:40 +0300

    rivers

--- app/Game/Generators/CellGenerator.php
+++ app/Game/Generators/CellGenerator.php
@@ -24,21 +24,27 @@ class CellGenerator
         };
         $height = $width;
 
+        // ground
         for ($x = 0; $x < $width; $x++) {
             for ($y = 0; $y < $height; $y++) {
                 $map[$x][$y] = 2;
             }
         }
 
+        // hills
         $hillsAreasCount = match ($size) {
             Size::x64 => random_int(20, 40),
             Size::x128 => random_int(80, 120),
             Size::x256 => random_int(200, 400),
         };
 
+        $hillsPoints = [];
+
         for ($i = 0; $i < $hillsAreasCount; $i++) {
             $x = random_int(0, $width-1);
             $y = random_int(0, $height-1);
+
+            $hillsPoints[] = [$x, $y];
 
             for ($j = 0; $j < 20; $j++) {
                 [$px, $py] = [$x, $y];
@@ -55,7 +61,7 @@ class CellGenerator
             }
         }
 
-
+        // mountains
         $mountainAreasCount = match ($size) {
             Size::x64 => random_int(5, 10),
             Size::x128 => random_int(20, 40),
@@ -63,8 +69,12 @@ class CellGenerator
         };
 
         for ($i = 0; $i < $mountainAreasCount; $i++) {
-            $x = random_int(0, $width-1);
-            $y = random_int(0, $height-1);
+            if (random_int(1, 100) < 80 && isset($hillsPoints[$i])) {
+                [$x, $y] = $hillsPoints[$i];
+            } else {
+                $x = random_int(0, $width-1);
+                $y = random_int(0, $height-1);
+            }
     
             $map[$px][$py] = 4;
 
@@ -82,34 +92,49 @@ class CellGenerator
             }
         }
 
-
+        // ocean and seas
         $waterAreasCount = match ($size) {
             Size::x64 => match ($water) {
-                Water::Low => random_int(1, 2),
-                Water::Medium => random_int(2, 4),
-                Water::High => random_int(4, 6),
+                Water::Low => 1,
+                Water::Medium => random_int(2, 3),
+                Water::High => random_int(3, 6),
             },
             Size::x128 => match ($water) {
-                Water::Low => random_int(4, 8),
-                Water::Medium => random_int(16, 24),
-                Water::High => random_int(32, 48),
+                Water::Low => random_int(5, 10),
+                Water::Medium => random_int(10, 15),
+                Water::High => random_int(20, 30),
             },
             Size::x256 => match ($water) {
-                Water::Low => random_int(10, 16),
-                Water::Medium => random_int(40, 80),
-                Water::High => random_int(70, 120),
+                Water::Low => random_int(10, 20),
+                Water::Medium => random_int(20, 40),
+                Water::High => random_int(40, 100),
             },
         };
 
+        $waterPoints = [];
+        $waterPointsIdx = -1;
+
         for ($i = 0; $i < $waterAreasCount; $i++) {
-            $x = random_int(0, $width-1);
-            $y = random_int(0, $height-1);
+            if (random_int(1, 100) < 40 && isset($waterPoints[$waterPointsIdx]) && is_array($waterPoints[$waterPointsIdx])) {
+                $x = $waterPoints[$waterPointsIdx][0] + [random_int(15,20), random_int(-20,-15)][random_int(0, 1)];
+                $x = max($x, 0);
+                $x = min($x, $width-1);
+                $y = $waterPoints[$waterPointsIdx][1] + [random_int(15,20), random_int(-20,-15)][random_int(0, 1)];
+                $y = max($y, 0);
+                $y = min($y, $width-1);
+                $waterPoints[] = [$x, $y];
+            } else {
+                $x = random_int(0, $width-1);
+                $y = random_int(0, $height-1);
+                $waterPoints[] = [$x, $y];
+                $waterPointsIdx++;
+            }
 
             $map[$x][$y] = 1;
-            for ($j = 0; $j < 80; $j++) {
+            for ($j = 0; $j < 200; $j++) {
                 [$px, $py] = [$x, $y];
                 
-                while (random_int(1, 100) > 2) {
+                while (random_int(1, 1000) > 15) {
                     if (random_int(1, 2) == 1) {
                         $px += random_int(-1, 1);
                     } else {
@@ -120,123 +145,122 @@ class CellGenerator
             }
         }
 
-        // $riversCount = match ($size) {
-        //     Size::x64 => match ($water) {
-        //         Water::Low => random_int(1, 2),
-        //         Water::Medium => random_int(2, 4),
-        //         Water::High => random_int(8, 12),
-        //     },
-        //     Size::x128 => match ($water) {
-        //         Water::Low => random_int(4, 6),
-        //         Water::Medium => random_int(8, 12),
-        //         Water::High => random_int(12, 20),
-        //     },
-        //     Size::x256 => match ($water) {
-        //         Water::Low => random_int(10, 16),
-        //         Water::Medium => random_int(20, 30),
-        //         Water::High => random_int(30, 40),
-        //     },
-        // };
+        // rivers
+        $riversCount = match ($size) {
+            Size::x64 => match ($water) {
+                Water::Low => random_int(4, 6),
+                Water::Medium => random_int(6, 12),
+                Water::High => random_int(12, 16),
+            },
+            Size::x128 => match ($water) {
+                Water::Low => random_int(12, 18),
+                Water::Medium => random_int(18, 24),
+                Water::High => random_int(24, 30),
+            },
+            Size::x256 => match ($water) {
+                Water::Low => random_int(12, 20),
+                Water::Medium => random_int(20, 30),
+                Water::High => random_int(30, 40),
+            },
+        };
 
-        // $riverLength = match ($size) {
-        //     Size::x64 => 64,
-        //     Size::x128 => 128,
-        //     Size::x256 => 256,
-        // };
+        $riverLength = match ($size) {
+            Size::x64 => 64,
+            Size::x128 => 128,
+            Size::x256 => 256,
+        };
 
-        // for ($i = 0; $i < $riversCount; $i++) {
-        //     $x = random_int(0, $width-1);
-        //     $y = random_int(0, $height-1);
+        for ($i = 0; $i < $riversCount; $i++) {
+            [$x, $y] = $waterPoints[random_int(0, count($waterPoints)-1)];
 
-        //     $map[$x][$y] = 1;
-        //     $riverWidth = random_int(2, 4);
-        //     $direction = random_int(1, 4);
+            $map[$x][$y] = 1;
+            $direction = random_int(1, 4);
 
-        //     [$px, $py] = [$x, $y];
+            while (random_int(0, $riverLength*10) > 10) {
+                if (random_int(0, 100) < 50) {
+                    $direction = [
+                        1 => [2,8],
+                        2 => [1,3],
+                        3 => [2,4],
+                        4 => [5,3],
+                        5 => [4,6],
+                        6 => [5,7],
+                        7 => [6,8],
+                        8 => [7,1],
+                    ][$direction][random_int(0, 1)];
+                }
 
-        //     $max = $riverLength;
+                $x = match($direction) {
+                    1 => $x+1,
+                    2 => $x+1,
+                    3 => $x,
+                    4 => $x-1,
+                    5 => $x-1,
+                    6 => $x-1,
+                    7 => $x,
+                    8 => $x+1,
+                };
+                $y = match($direction) {
+                    1 => $y,
+                    2 => $y+1,
+                    3 => $y+1,
+                    4 => $y+1,
+                    5 => $y,
+                    6 => $y-1,
+                    7 => $y-1,
+                    8 => $y-1,
+                    default => $y
+                };
 
-        //     while ($max > 0) {
-        //         $max--;
-        //         if (random_int(1, 100) < 40) {
-        //             if ($riverWidth == 4) {
-        //                 $riverWidth--;
-        //             } else if ($riverWidth == 0) {
-        //                 $riverWidth++;
-        //             } else {
-        //                 $riverWidth += random_int(-1, 1);
-        //             }
-        //         }
-        //         if (random_int(1, 100) < 40) {
-        //             $direction = [
-        //                 1 => [2,4],
-        //                 2 => [1,3],
-        //                 3 => [2,4],
-        //                 4 => [1,3]
-        //             ][$direction][random_int(0,1)];
-        //         }
+                if ($x < 0 || $x >= $width || $y < 0 || $y >= $height) {
+                    break;
+                }
 
-        //         switch ($direction) {
-        //             case 1:
-        //                 // top -> bottom
-        //                 $py++;
+                $map[$x][$y] = 1;
+            }
+        }
 
-        //                 $left = $px - floor($riverWidth / 2);
-        //                 $right = $px + floor($riverWidth / 2);
+        // smoothing
+        for ($i = 0; $i < 5; $i++) {
+            for ($x = 1; $x < $width-1; $x++) {
+                for ($y = 1; $y < $height-1; $y++) {
+                    $neighbors = [
+                        $map[$x-1][$y-1],
+                        $map[$x-1][$y],
+                        $map[$x-1][$y+1],
+                        $map[$x][$y-1],
+                        $map[$x][$y],
+                        $map[$x][$y+1],
+                        $map[$x+1][$y-1],
+                        $map[$x+1][$y],
+                        $map[$x+1][$y+1],
+                    ];
 
-        //                 while ($left <= $right) {
-        //                     if (key_exists($left, $map) && key_exists($right, $map)) {
-        //                         $map[$left][$py] = 1;
-        //                     }
-        //                     $left++;
-        //                 }
-        //             break;
-        //             case 2:
-        //                 // right -> left
-        //                 $px--;
+                    $counts = [];
+                    foreach ($neighbors as $type) {
+                        $counts[$type] = isset($counts[$type]) ? $counts[$type] + 1 : 1;
+                    }
 
-        //                 $top = $py - floor($riverWidth / 2);
-        //                 $bottom = $py + floor($riverWidth / 2);
+                    $maxType = array_keys($counts)[0];
+                    foreach ($counts as $type => $count) {
+                        if ($count > $counts[$maxType]) {
+                            $maxType = $type;
+                        }
+                    }
 
-        //                 while ($top <= $bottom) {
-        //                     if (key_exists($top, $map) && key_exists($bottom, $map)) {
-        //                         $map[$px][$top] = 1;
-        //                     }
-        //                     $top++;
-        //                 }
-        //             break;
-        //             case 3:
-        //                 // bottom -> top
-        //                 $py--;
+                    if ($counts[$maxType] > 6) {
+                        $map[$x][$y] = $maxType;
+                    }
+                }
+            }
+        }
 
-        //                 $left = $px - floor($riverWidth / 2);
-        //                 $right = $px + floor($riverWidth / 2);
 
-        //                 while ($left <= $right) {
-        //                     if (key_exists($left, $map) && key_exists($right, $map)) {
-        //                         $map[$left][$py] = 1;
-        //                     }
-        //                     $left++;
-        //                 }
-        //             break;
-        //             case 4:
-        //                 // left -> right
-        //                 $px++;
-
-        //                 $top = $py - floor($riverWidth / 2);
-        //                 $bottom = $py + floor($riverWidth / 2);
-
-        //                 while ($top <= $bottom) {
-        //                     if (key_exists($top, $map) && key_exists($bottom, $map)) {
-        //                         $map[$px][$top] = 1;
-        //                     }
-        //                     $top++;
-        //                 }
-        //             break;
-        //         }
-        //     }
-        // }
-
+        foreach ($map as $i => $row) {
+            if (count($row) > $height) {
+                $map[$i] = array_slice($row, 0, $height);
+            }
+        }
 
         return $map;
     }
--- resources/js/Components/Pages/Game.vue
+++ resources/js/Components/Pages/Game.vue
@@ -14,10 +14,6 @@ let players = [];
 let groundCanvas = null
 let ctx = null
 
-let groundCanvasStyle = computed(() => {
-    return `width: ${map.ground.length*32}px; height: ${map.ground.length*32}px;`
-})
-
 onMounted(() => {
     // @ts-ignore
     groundCanvas = document.getElementById("ground");
@@ -27,7 +23,7 @@ onMounted(() => {
 
 
 let drawMap = function() {
-    let cellSize = 16;
+    let cellSize = 48;
 
     groundCanvas.style.width = `${map.ground.length * cellSize}px`;
     groundCanvas.width = map.ground.length * cellSize;
@@ -73,22 +69,108 @@ wsmanager.subscribe(WsChannel.Game, (type: string, payload: any) => {
     }
 })
 
+
+onMounted(() => {
+    let map = document.getElementById("map")
+
+    let mapDragging = false;
+    let startX : number, startY : number;
+    let offsetX: number, offsetY: number;
+
+    let scale = 1;
+    const minScale = 0.1;
+    const maxScale = 2;
+
+    if (map !== null) {
+        map.addEventListener("mousedown", (event) => {
+            if (event.button == 2) {
+                mapDragging = true;
+
+                startX = event.clientX
+                startY = event.clientY
+                offsetX = map.offsetLeft
+                offsetY = map.offsetTop
+            }
+        })
+        document.addEventListener("mousemove", (event) => {
+            if (mapDragging) {
+                let deltaX = event.clientX - startX;
+                let deltaY = event.clientY - startY;
+
+                let newOffsetX = offsetX + deltaX;
+                let newOffsetY = offsetY + deltaY;
+
+                const minX = -((map.scrollWidth * scale) - (window.innerWidth / 2));
+                const maxX = window.innerWidth / 2;
+                const minY = -((map.scrollHeight * scale) - (window.innerHeight / 2));
+                const maxY = window.innerHeight / 2;
+
+                // Установка новых значений с учетом ограничений
+                map.style.left = Math.max(minX, Math.min(newOffsetX, maxX)) + "px";
+                map.style.top = Math.max(minY, Math.min(newOffsetY, maxY)) + "px";
+            }
+        });
+        document.addEventListener("mouseup", (event) => {
+            if (event.button === 2) {
+                mapDragging = false;
+            }
+        })
+
+        document.addEventListener("wheel", (event) => {
+            event.preventDefault();
+
+            let rect = map.getBoundingClientRect();
+            let mouseX = event.clientX - rect.left;
+            let mouseY = event.clientY - rect.top;
+
+            let oldScale = scale;
+            let scaleStep = 0.1;
+            scale += event.deltaY < 0 ? scaleStep : -scaleStep;
+            scale = Math.max(minScale, Math.min(scale, maxScale));
+
+            let scaleRatio = scale / oldScale;
+
+            let newLeft = event.clientX - (mouseX * scaleRatio);
+            let newTop = event.clientY - (mouseY * scaleRatio);
+
+            const minX = -(map.offsetWidth * scale - window.innerWidth / 2);
+            const maxX = window.innerWidth / 2;
+            const minY = -(map.offsetHeight * scale - window.innerHeight / 2);
+            const maxY = window.innerHeight / 2;
+
+            map.style.left = `${Math.max(minX, Math.min(newLeft, maxX))}px`;
+            map.style.top = `${Math.max(minY, Math.min(newTop, maxY))}px`;
+            map.style.transformOrigin = "0 0";
+            map.style.transform = `scale(${scale})`;
+        });
+    }
+})
+
+
 </script>
 
 <template>
     <div class="game">
-        <div class="map">
+        <div id="map">
             <canvas id="ground"></canvas>
         </div>
     </div>
 </template>
 
-<style lang="scss">
+<style lang="scss" scoped>
 .game {
-    width: 100%;
-    height: 100%;
+    width: 100vw;
+    height: 100vh;
+    position: relative;
 }
 #ground {
     
+}
+#map {
+    width: fit-content;
+    height: fit-content;
+    position: absolute;
+    top: 0;
+    left: 0;
 }
 </style>
